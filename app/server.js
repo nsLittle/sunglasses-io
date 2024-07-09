c283c1edf42c6958b09c47b0b208d4766bea9e1b
nsLittle <notsolittle88@gmail.com>
2024-07-09 08:30:36 -0400

    initial commit; three passing tests

--- app/server.js
+++ app/server.js
@@ -28,4 +28,29 @@ app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
 
+
+app.get('/brands', (req, res) => {
+	res.json(brands);
+});
+
+app.get('/products', (req, res) => {
+	res.json(products);
+	res.json(products.name);
+});
+
+app.get('/products/:name', (req, res) => {
+	const productName = req.params.name;
+	const product = products.find(p => p.name === productName);
+
+	if (prodcut) {
+		res.json(product)
+	} else {
+		res.status(404).send('Product not found');
+	};
+});
+
+app.get('/users', (req, res) => {
+	res.json(users);
+});
+
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -5,9 +5,37 @@ const server = require('../app/server'); // Adjust the path as needed
 const should = chai.should();
 chai.use(chaiHttp);
 
+// Importing the data from JSON files
+const users = require('../initial-data/users.json');
+const brands = require('../initial-data/brands.json');
+const products = require('../initial-data/products.json');
+
 // TODO: Write tests for the server
 
-describe('Brands', () => {});
+describe('Brands', () => {
+  describe('GETS all brand naames', () => {
+    it('should return all brands', (done) => {
+      assert.equal(brands.length, 5);
+      done();
+    });
+  });
+});
+
+describe('Products', () => {
+  describe('GETS all products', () => {
+    it('should return all products', (done) => {
+      assert.equal(products.length, 11);
+      done();
+    });
+  });
+  describe('GETS specified', () => {
+    it('should return specified product', (done) => {
+      const product = products.find(p => p.id === "11");
+      assert.equal(product.name, "Habanero");
+      done();
+    });
+  });
+});
 
 describe('Login', () => {});
 
